f141703db91e81503743aaa7bacb8602bcb93c69
zineb zarda <zibevzarda1@gmail.com>
2024-02-11 16:28:40 +0100

    Add student

--- src/Library.java
+++ src/Library.java
@@ -4,7 +4,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Library {
-
+    // ANSI escape codes for colors
+    public static final  String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
     public ArrayList<Book> LibraryBooks = new ArrayList<Book>();
     public ArrayList<Student> LibraryStudents = new ArrayList<Student>();
     Scanner scanner = new Scanner(System.in);
@@ -13,7 +22,7 @@ public class Library {
 
     //Add Book
     public void addBook() {
-        System.out.print("Title :");
+        System.out.print(ANSI_YELLOW+"Title :");
         String Title = scanner.next();
         System.out.print("Writer :");
         String Writer = scanner.next();
@@ -33,6 +42,23 @@ public class Library {
             Book B = new Book(Title,Writer,ISBN,DatePublication);
             LibraryBooks.add(B);
         }
+
+    //Add Student
+
+    public void addStudent() {
+        System.out.print(ANSI_YELLOW+"Nom :");
+        String Nom = scanner.next();
+        System.out.print("Prenom :");
+        String Prenom = scanner.next();
+        System.out.print("Adresse :");
+        String Adresse = scanner.next();
+        Student S = new Student(Nom, Prenom, Adresse);
+        LibraryStudents.add(S);
     }
 
-}
+
+
+
+    }
+
+
